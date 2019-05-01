24313582c0ad4b22466b83f1fdde8b45f8300133
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-05-01 15:58:21 +0100

    Deploy

--- start_server.sh
+++ start_server.sh
@@ -38,7 +38,7 @@ docker build -t doe .
 # Run container
 if [ "$DEPLOY" == "true" ]; then
     docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-    docker run --name doe -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/appoptdes -e VIRTUAL_HOST=doe.synbiochem.co.uk doe
+    docker run --name doe -d -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/appoptdes -e VIRTUAL_HOST=doe.synbiochem.co.uk doe
 else
     docker run --name doe -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/appoptdes doe
 fi
