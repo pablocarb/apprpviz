97c50a6d34513cdd08b356e1f460d24790fe8ebc
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-05-01 15:19:56 +0100

    Docker updates

--- start_server.sh
+++ start_server.sh
@@ -30,10 +30,8 @@ docker build -t doe .
 # Run container
 if [ "$DEPLOY" == "true" ]; then
     docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-    # Run container with user's uid to avoid permission issues when updating the repository
-    docker run -u `id -u $USER` --name doe -d -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $CWD:/appoptdes doe -e VIRTUAL_HOST=doe.synbiochem.co.uk 
+    docker run --name doe -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/appoptdes -e VIRTUAL_HOST=doe.synbiochem.co.uk doe
 else
-    # Run container with user's uid to avoid permission issues when updating the repository
-    docker run -u `id -u $USER` --name doe -d -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $CWD:/appoptdes doe 
+    docker run --name doe -p 8989:8989 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/appoptdes doe
 fi
 
