4530f3faff07389272cb279ddb1fb0cc6c13967b
mibsspc2 <1038612+pablocarb@users.noreply.github.com>
2019-06-13 14:41:26 +0100

    Small fixes

--- start_server.sh
+++ start_server.sh
@@ -18,9 +18,9 @@ docker stop nginx-proxy
 docker rm nginx-proxy
 
 # Stop an delete doe containers
-docker stop rpviz
-docker rm rpviz
-docker rmi rpviz
+docker stop $LOCALREPO
+docker rm $LOCALREPO
+docker rmi $LOCALREPO
 
 # Update repository and change to production branch
 if [ ! -d $LOCALREPO ]
@@ -33,13 +33,13 @@ else
 fi
 
 # Build new image
-docker build -t rpviz .
+docker build -t $LOCALREPO .
 
 # Run container
 if [ "$DEPLOY" == "true" ]; then
     docker run --name nginx-proxy -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
-    docker run --name rpviz -d -p 8998:8998 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/apprpviz -e VIRTUAL_HOST=rpvi\.synbiochem.co.uk doe
+    docker run --name $LOCALREPO -d -p 8998:8998 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/apprpviz -e VIRTUAL_HOST=rpviz.synbiochem.co.uk $LOCALREPO
 else
-    docker run --name doe -p 8998:8998 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/apprpviz doe
+    docker run --name $LOCALREPO -p 8998:8998 -e LD_LIBRARY_PATH='/opt/conda/bin/../lib' -v $PWD:/apprpviz $LOCALREPO
 fi
 
